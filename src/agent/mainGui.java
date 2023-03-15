c84206018cbd15a0f0a4af2c951fc730739fa6fc
NaGaSu <kitti.chiewchan@gmail.com>
2023-03-15 16:30:29 +0700

    First update mainGui Demo version

---
+++ src/agent/mainGui.java
@@ -0,0 +1,14 @@
+package agent;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class mainGui extends JFrame {
+    private customerAgent customerAgent;
+    private specialistAgent specialistAgent;
+    private supplierAgent supplierAgent;
+
+    //Gui design preferences
+    private JTextArea log;
+    mainGui()
+}
